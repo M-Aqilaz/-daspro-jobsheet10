a9eaa70a62b5d0012753d14bd3a42abf24348f7d
M-Aqilaz <muhaaqil6002@gmail.com>
2024-11-07 07:55:18 +0700

    com4

--- Bioskop16.java
+++ Bioskop16.java
@@ -17,8 +17,14 @@ public class Bioskop16 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang Baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (String[] barispenonton : penonton) {
+            System.out.println("Panjang Baris: " + barispenonton.length);
+        }
+        
+        System.out.println("Penonton Baris ke-3: ");
+
+        for (int i = 0; penonton[2].length < 10; i++) {
+            System.out.println(penonton[2][i]);
         }
     }
 }
