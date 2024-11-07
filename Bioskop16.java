6fb64739bd57cffc8f833659d1cc72be56b35c58
M-Aqilaz <muhaaqil6002@gmail.com>
2024-11-07 08:04:58 +0700

    com6

--- Bioskop16.java
+++ Bioskop16.java
@@ -26,5 +26,9 @@ public class Bioskop16 {
         for (String i : penonton[2]) {
             System.out.println(i);
         }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton Baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]) );
+        }
     }
 }
